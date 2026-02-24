dd0ac903a49e8ced4b1ccb19532abdf081188ee2
aleriverazz <alejandra.cristina.rivera@students.iaac.net>
2026-02-24 17:10:55 +0100

    Final base and basename fixes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mdef_website">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
